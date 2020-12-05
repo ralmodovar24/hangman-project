5b90a43077eece53d51cd066335d6875d2c2dfcb
Ricardo Almodovar <ralmodovar02@gmail.com>
2020-12-05 18:45:03 -0400

    *

--- JangMan.java
+++ JangMan.java
@@ -7,9 +7,7 @@ import java.util.Scanner;
 
 public class JangMan {
 
-
     public ArrayList<String> letters = new ArrayList<>();
-
 
     /*
      * Metodo para adquirir respuesta del usuario y comparar. Aqui estaba probando
@@ -18,43 +16,32 @@ public class JangMan {
     private static boolean getAns(Scanner scanner, String ch, ArrayList<String> letters) {
         System.out.println("Enter your guess: ");
         String inputString = scanner.next();
-        
+
         ch.length();
         for (int i = 0; i < ch.length(); i++) {
             System.out.print("X");
         }
         System.out.println();
-        if(ch.contains(inputString)){
+        if (ch.contains(inputString)) {
             int index = ch.indexOf(inputString);
-            letters.add(index, inputString);
+
         }
-/*
-        System.out.println(letters.iterator());
-        int idx = ch.indexOf(inputString);
-        System.out.println(idx);
-
-        if (!letters.contains(inputString)) {
-            // we check if word to find contains c
-            if (ch.contains(inputString)) {
-                 // if so, we replace _ by the character c
-                int index = ch.indexOf(inputString);
-
-                while (index >= 0) {
-                    wordFound[index] = c.charAt(0);
-                    index = wordToFind.indexOf(c, index + 1);
-                }
-            } else {
-                // c not in the word => error
-                nbErrors++;
-            }
-
-            // c is now a letter entered
-            letters.add(c);
-        }
-*/
+        /*
+         * System.out.println(letters.iterator()); int idx = ch.indexOf(inputString);
+         * System.out.println(idx);
+         * 
+         * if (!letters.contains(inputString)) { // we check if word to find contains c
+         * if (ch.contains(inputString)) { // if so, we replace _ by the character c int
+         * index = ch.indexOf(inputString);
+         * 
+         * while (index >= 0) { wordFound[index] = c.charAt(0); index =
+         * wordToFind.indexOf(c, index + 1); } } else { // c not in the word => error
+         * nbErrors++; }
+         * 
+         * // c is now a letter entered letters.add(c); }
+         */
         return ch.contains(inputString);
     }
-
 
     /*
      * Metodo para leer las palabras de un file usando br. En este estoy tratando de
@@ -68,7 +55,6 @@ public class JangMan {
         Random r = new Random();
         int randomNum = r.nextInt(10);
 
-        
         while (line != null) {
 
             if (count == randomNum) {
@@ -80,35 +66,30 @@ public class JangMan {
             line = br.readLine();
 
         }
-        
+
         return palabraEscojida;
     }
 
-
     private static void scorePrnt(Scanner scanner, int cnt, BufferedReader buffer, StringBuilder sb, String ch,
-            ArrayList<String> letters)
-            throws IOException {
-        if (cnt != 6) 
-        {
+            ArrayList<String> letters) throws IOException {
+        if (cnt != 6) {
             if (getAns(scanner, ch, letters) == false) {
                 System.out.println("Intento fallido");
-                cnt ++;
-                //System.out.println(cnt);
+                cnt++;
+                // System.out.println(cnt);
                 firstTurn(buffer, sb, cnt, scanner, ch, letters);
             } else {
                 System.out.println("Intento valido");
                 firstTurn(buffer, sb, cnt, scanner, ch, letters);
             }
-        } else{
+        } else {
             System.out.println("Perdiste!!");
             System.exit(0);
         }
     }
 
-
     private static void firstTurn(BufferedReader buffer, StringBuilder sb, int cnt, Scanner scanner, String ch,
-            ArrayList<String> letters)
-            throws IOException {
+            ArrayList<String> letters) throws IOException {
 
         String line = buffer.readLine();
         while (line != null) {
@@ -122,7 +103,6 @@ public class JangMan {
         scorePrnt(scanner, cnt, buffer, sb, ch, letters);
 
     }
-
 
     public static void main(String[] args) throws IOException {
 
