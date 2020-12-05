be5a5b6856c6bc437de69b82f4a3cc06b420b561
EduardoTorres <torreseduardo804@gmail.com>
2020-12-05 17:34:38 -0400

    Con Online

--- JangMan.java
+++ JangMan.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class JangMan {
 
+    private ArrayList<String> letters = new ArrayList<>();
+    //final int MAXERROR = 7;
     /*
      * Metodo para adquirir respuesta del usuario y comparar. Aqui estaba probando
      * el getAns con un hard code de la respuesta "L"
@@ -13,11 +16,12 @@ public class JangMan {
     private static boolean getAns(Scanner scanner, String ch) {
         System.out.println("Enter your guess: ");
         String inputString = scanner.next();
-
+        
         ch.length();
         for (int i = 0; i < ch.length(); i++) {
-            System.out.println("X");
+            System.out.print("X");
         }
+        System.out.println();
         //int idx = ch.indexOf(inputString);
         //System.out.println(idx);
 
@@ -67,7 +71,7 @@ public class JangMan {
             if (getAns(scanner, ch) == false) {
                 System.out.println("Intento fallido");
                 cnt += 9;
-                System.out.println(cnt);
+                //System.out.println(cnt);
                 firstTurn(buffer, sb, cnt, scanner, ch);
             } else {
                 System.out.println("Intento valido");
@@ -81,9 +85,6 @@ public class JangMan {
 
     private static void firstTurn(BufferedReader buffer, StringBuilder sb, int cnt, Scanner scanner, String ch)
             throws IOException {
-        // String line = buffer.readLine();
-        // String fileAsString = sb.toString();
-        // String[] line = fileAsString.split("[=========]", 0);
 
         String line = buffer.readLine();
         while (line != null) {
@@ -95,11 +96,6 @@ public class JangMan {
         String[] arrOfStr = fileAsString.split("[=========]", -1);
         System.out.println(arrOfStr[cnt]);
         scorePrnt(scanner, cnt, buffer, sb, ch);
-        // for (int i = 0; i < arrOfStr.length-1; i++)
-        // {
-        // System.out.println(arrOfStr[i]);
-
-        // }
 
     }
 
